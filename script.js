cf65d0b490c150ad7bf50cb4b7011cb08157926f
adznshfy <jihaan.shaafiya@gmail.com>
2025-06-03 23:15:32 +0700

    final

--- script.js
+++ script.js
@@ -40,3 +40,42 @@ AOS.init();
 
   loadQuote();
   setInterval(loadQuote, 10000);
+
+// Inisialisasi Swiper untuk bagian proyek
+const projectSwiper = new Swiper('.project-swiper', {
+  // Optional parameters
+  loop: true, // Untuk membuat carousel berputar tak terbatas
+  slidesPerView: 1, // Menampilkan 1 slide per tampilan secara default
+  spaceBetween: 30, // Jarak antar slide
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Pagination bisa diklik untuk navigasi
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2, // Menampilkan 2 slide saat lebar layar >= 640px
+      spaceBetween: 20
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3, // Menampilkan 3 slide saat lebar layar >= 768px
+      spaceBetween: 30
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4, // Menampilkan 4 slide saat lebar layar >= 1024px
+      spaceBetween: 40
+    }
+  }
+});
